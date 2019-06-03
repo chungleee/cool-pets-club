5f9962f37f33fd8f787f7c4186a733a9494a02cf
Leon <leon.karho.chung@gmail.com>
2019-06-03 16:20:45 -0400

    /create route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
+const User = require('../models/User')
 
-router.get('/test', (req, res) => {
-  return res.json({ test: true })
+// @route POST /api/users/create
+// @desc  Create new user
+// @acc   Public
+router.post('/create', async (req, res) => {
+  const { email, password } = req.body
+  try {
+    return res.json({ email, password })
+  } catch (error) {
+    console.error(error)
+    return res.status(400).json(error)
+  }
 })
 
 module.exports = router
