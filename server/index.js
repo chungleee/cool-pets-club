19b0bbc195e273fd96b51baa015f33f50059281d
Leon <leon.karho.chung@gmail.com>
2019-06-03 16:00:40 -0400

    mongoose settings to mlab

--- server/index.js
+++ server/index.js
@@ -1,16 +1,33 @@
+// imports
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
 
+// server settings
+const app = express()
+const port = process.env.PORT || 3000
+const { MONGO_URI } = require('./config/keys_dev')
+
+// api routes
 const usersRoutes = require('./routes/users')
 
-const port = process.env.PORT || 3000
-
+// middlewares
 app.use('/api/users', usersRoutes)
 
+// test route
 app.get('/', (req, res) => {
   return res.json({ success: true })
 })
 
+// connections
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log('>>> connected to mongoDB')
+  })
+  .catch(error => {
+    console.error(error)
+    process.exit()
+  })
 app.listen(port, () => {
   console.log(`>>> server running on http://localhost:${port}`)
 })
