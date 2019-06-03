27af4e2837f45f3f409e609f2447ea36b6508125
Leon <leon.karho.chung@gmail.com>
2019-06-03 17:00:58 -0400

    add bcryptjs - salt & hash password

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
 const User = require('../models/User')
 const { omitPassword } = require('../utils/utilities')
 
@@ -15,10 +16,14 @@ router.post('/create', async (req, res) => {
     if (user) {
       return res.status(400).json({ error: 'Email is already in use' })
     }
+    // salt & hash password
+    const salt = await bcrypt.genSalt(12)
+    const hash = await bcrypt.hash(password, salt)
+
     // if don't exists - create new user
     let newUser = await new User({
       email,
-      password
+      password: hash
     })
     // save and return document
     newUser = await newUser.save()
