e240f64f08a6abd63fc645c183c6736f63f8162e
Leon <leon.karho.chung@gmail.com>
2019-06-07 14:41:24 -0400

    find user from decoded token id - remove password from returned user doc - attach user to req

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
+const { omitPassword } = require('../utils/utilities')
 const { JWT_SECRET } = require('../config/keys_dev')
 
 const auth = async (req, res, next) => {
@@ -14,8 +15,15 @@ const auth = async (req, res, next) => {
     const token = req.header('Authorization').replace('Bearer ', '')
     // decode token
     const decoded = await jwt.verify(token, JWT_SECRET)
-    console.log('decoded', decoded)
 
+    // find user by id
+    let user = await User.findById(decoded.id)
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' })
+    }
+
+    user = omitPassword(user.toObject(), 'password')
+    req.user = user
     next()
   } catch (error) {
     console.error(error)
