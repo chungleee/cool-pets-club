77bdd0039db741067aeb55563e5c2975440dfc41
Leon <leon.karho.chung@gmail.com>
2019-06-19 16:45:26 -0400

    fix layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container } from '@material-ui/core'
+import { Container, CssBaseline } from '@material-ui/core'
 import Login from './components/Login'
 import Register from './components/Register'
 
 const App = () => {
   return (
-    <Container>
-      <Router>
-        <Route path="/" exact component={Login} />
-        <Route path="/register" component={Register} />
-      </Router>
+    <Container maxWidth="xs">
+      <CssBaseline>
+        <Router>
+          <Route path="/" exact component={Login} />
+          <Route path="/register" component={Register} />
+        </Router>
+      </CssBaseline>
     </Container>
   )
 }
