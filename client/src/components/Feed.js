2cbef4347666b6403d293813a9f71d65fc351375
Leon <leon.karho.chung@gmail.com>
2019-06-22 17:21:59 -0400

    styling && fontawesome icons

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -5,23 +5,32 @@ import {
   Typography,
   GridList,
   GridListTile,
-  Button,
+  Icon,
   IconButton
 } from '@material-ui/core'
+
+const style = {
+  Toolbar: { justifyContent: 'space-between' },
+  GridList: { paddingTop: '100px' }
+}
 
 const Feed = () => {
   return (
     <Fragment>
       <AppBar position="fixed" color="default">
-        <Toolbar>
+        <Toolbar style={style.Toolbar}>
           <Typography variant="body1">Cool Pets Group</Typography>
-          <IconButton>+</IconButton>
+          <div>
+            <IconButton>
+              <Icon className="fas fa-camera-retro" />
+            </IconButton>
+            <IconButton>
+              <Icon className="fas fa-user-circle" />
+            </IconButton>
+          </div>
         </Toolbar>
       </AppBar>
-      <GridList style={{ paddingTop: '100px' }} cellHeight={150} cols={3}>
-        <GridListTile>
-          <img src="https://picsum.photos/id/237/150/150" alt="picture" />
-        </GridListTile>
+      <GridList style={style.GridList} cellHeight={160} cols={3}>
         <GridListTile>
           <img src="https://picsum.photos/id/237/150/150" alt="picture" />
         </GridListTile>
