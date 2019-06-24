a5dd551e90311cc8fb965aad229e7393610a6348
Leon <leon.karho.chung@gmail.com>
2019-06-24 12:09:40 -0400

    remove Container - set login route

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container, CssBaseline } from '@material-ui/core'
+import { CssBaseline } from '@material-ui/core'
 import Login from './components/Login'
 import Register from './components/Register'
 import Feed from './components/Feed'
@@ -9,17 +9,15 @@ import Camera from './components/Camera'
 
 const App = () => {
   return (
-    <Container maxWidth="md">
-      <CssBaseline>
-        <Router>
-          <Route exact path="/" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/feed" component={Feed} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/camera" component={Camera} />
-        </Router>
-      </CssBaseline>
-    </Container>
+    <CssBaseline>
+      <Router>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/feed" component={Feed} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/camera" component={Camera} />
+      </Router>
+    </CssBaseline>
   )
 }
 
