e8c69254b0245b05b2daff9f316ddf95d1dd7926
Leon <leon.karho.chung@gmail.com>
2019-06-24 12:10:07 -0400

    remove Appbar - include Layout

--- client/src/components/Feed.js
+++ client/src/components/Feed.js
@@ -1,41 +1,26 @@
-import React, { Fragment } from 'react'
-import { Link } from 'react-router-dom'
-import {
-  AppBar,
-  Toolbar,
-  Typography,
-  GridList,
-  GridListTile,
-  Icon,
-  IconButton
-} from '@material-ui/core'
-
-const style = {
-  Toolbar: { justifyContent: 'space-between' },
-  GridList: { paddingTop: '100px' }
-}
+import React from 'react'
+import { GridList, GridListTile } from '@material-ui/core'
+import Layout from './Layout'
 
 const Feed = () => {
   return (
-    <Fragment>
-      <AppBar position="fixed" color="default">
-        <Toolbar style={style.Toolbar}>
-          <Typography variant="body1">Cool Pets Group</Typography>
-          <div>
-            <Link to="/camera">
-              <IconButton>
-                <Icon className="fas fa-camera-retro" />
-              </IconButton>
-            </Link>
-            <Link to="/profile">
-              <IconButton>
-                <Icon className="fas fa-user-circle" />
-              </IconButton>
-            </Link>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <GridList style={style.GridList} cellHeight={160} cols={3}>
+    <Layout>
+      <GridList spacing={2} cellHeight={100} cols={3}>
+        <GridListTile>
+          <img src="https://picsum.photos/id/237/150/150" alt="picture" />
+        </GridListTile>
+        <GridListTile>
+          <img src="https://picsum.photos/id/237/150/150" alt="picture" />
+        </GridListTile>
+        <GridListTile>
+          <img src="https://picsum.photos/id/237/150/150" alt="picture" />
+        </GridListTile>
+        <GridListTile>
+          <img src="https://picsum.photos/id/237/150/150" alt="picture" />
+        </GridListTile>
+        <GridListTile>
+          <img src="https://picsum.photos/id/237/150/150" alt="picture" />
+        </GridListTile>
         <GridListTile>
           <img src="https://picsum.photos/id/237/150/150" alt="picture" />
         </GridListTile>
@@ -76,7 +61,7 @@ const Feed = () => {
           <img src="https://picsum.photos/id/237/150/150" alt="picture" />
         </GridListTile>
       </GridList>
-    </Fragment>
+    </Layout>
   )
 }
 
